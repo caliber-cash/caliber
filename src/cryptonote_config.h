5fbb370197bb2dd20e00e8e16d01fd6a2bc44292
caliber-cash <calibercurrency@gmail.com>
2018-10-19 01:11:44 +1300

    Generate genesis blocks for Testnet and Stagenet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -170,7 +170,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x69, 0x04 , 0x46, 0x61, 0x17, 0x31, 0x04, 0x82, 0x11, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+    std::string const GENESIS_TX = "023c01ff00018080c8dcbda1dfbf02026e3323d2df2c61ce60ea66d053616a4b8abfeb01f137d26359fb7471bd0684b82101b3f879b2af64e4daf4d1b491a0bec945798a3ea8894a5d4f21f4dae580cb80c600";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
@@ -185,7 +185,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x60, 0x12, 0x30, 0x00, 0x82, 0x12, 0xA1, 0xA1, 0x12
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+    std::string const GENESIS_TX = "023c01ff00018080c8dcbda1dfbf02027807cd0e921a72204b8fb37f999d7aca0271eb36b403bf3b3ea1634499371f622101e9f5da74147d0cac2685183d90f367125791dfa66d820876c33ca11306e1ed8200";
     uint32_t const GENESIS_NONCE = 10002;
   }
 }
